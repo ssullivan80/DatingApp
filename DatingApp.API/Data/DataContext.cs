7861c877e9139f0df3e954ac6ecee037e36621a1
Sean Sullivan <sean.david.sullivan@gmail.com>
2020-07-15 14:21:51 -0400

    end of section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -13,6 +13,8 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values {get;set;}
 
         public DbSet<User> Users {get;set;}
+
+        public DbSet<Photo> Photos {get;set;}
         
     }
 }
